a98404e02d7094e516f909052b30932eefcd582a
bradhead <brad.head@gmail.com>
2024-01-17 13:26:45 -0800

    updates.

--- smart-on-fhir-spi/src/main/java/com/zedwerks/keycloak/authenticators/SmartEhrLaunchAuthenticatorFactory.java
+++ smart-on-fhir-spi/src/main/java/com/zedwerks/keycloak/authenticators/SmartEhrLaunchAuthenticatorFactory.java
@@ -8,27 +8,39 @@ import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
 import org.keycloak.provider.ProviderConfigProperty;
 
+import com.fasterxml.jackson.databind.introspect.AccessorNamingStrategy.Provider;
+
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class SmartEhrLaunchAuthenticatorFactory implements AuthenticatorFactory {
     private static final String PROVIDER_ID = "smart-ehr-launch";
+
     private static final String LAUNCH_PROP_NAME = "launch";
     private static final String LAUNCH_PROP_LABEL = "EHR-Launch";
-    private static final String LAUNCH_PROP_DESCRIPTION = "The EHR-Launch parameter name";
+    private static final String LAUNCH_PROP_HELPTEXT= "The EHR-Launch parameter name";
 
     // Configuration Settings to connect to the Context API server
     public static final String CONF_CONTEXT_API_URL = "context-api-url";
     public static final String CONF_CONTEXT_API_URL_LABEL = "Context API URL";
-    public static final String CONF_CONTEXT_API_URL_DESCRIPTION = "The URL of the Context API server";
+    public static final String CONF_CONTEXT_API_URL_HELPTEXT = "The URL of the Context API server";
+
     public static final String CONF_CONTEXT_ISS_URL = "context-iss-url";
-    public static final String CONF_CONTEXT_ISS_URL_LABEL = "Context Issuer (iss) URL";
+    public static final String CONF_CONTEXT_ISS_URL_LABEL = "Issuer URL";
+    public static final String CONF_CONTEXT_ISS_URL_HELPTEXT = "The Base Issuer URL";
+
     public static final String CONF_ISS_CLIENT_ID = "context-client-id";
-    public static final String CONF_ISS_CLIENT_ID_LABEL = "Context Service OAuth2 Client ID";
+    public static final String CONF_ISS_CLIENT_ID_LABEL = "Client ID";
+    public static final String CONF_ISS_CLIENT_ID_HELPTEXT = "The Client ID for this client of the Context Service";
+
     public static final String CONF_ISS_CLIENT_SECRET = "context-client-secret";
-    public static final String CONF_ISS_CLIENT_SECRET_LABEL = "Context Service OAuth2 Client Secret";
+    public static final String CONF_ISS_CLIENT_SECRET_LABEL = "Client Secret";
+    public static final String CONF_ISS_CLIENT_SECRET_HELPTEXT = "THE Context Service Client Secret";
+
     public static final String CONF_ISS_CLIENT_SCOPE = "context-client-scope";
-    public static final String CONF_ISS_CLIENT_SCOPE_LABEL = "Context Service OAuth2 Client Scope(s) space-delimited";
+    public static final String CONF_ISS_CLIENT_SCOPE_LABEL = "Scopes";
+    public static final String CONF_ISS_CLIENT_SCOPE_HELPTEXT = "Context Service OAuth2 Client Scope(s) space-delimited";
 
     @Override
     public String getDisplayType() {
@@ -66,8 +78,21 @@ public class SmartEhrLaunchAuthenticatorFactory implements AuthenticatorFactory 
 
     @Override
     public List<ProviderConfigProperty> getConfigProperties() {
-        return Collections.singletonList(new ProviderConfigProperty(LAUNCH_PROP_NAME, LAUNCH_PROP_LABEL,
-                LAUNCH_PROP_DESCRIPTION, ProviderConfigProperty.MULTIVALUED_STRING_TYPE, null));
+
+        ProviderConfigProperty contextApiUrl = new ProviderConfigProperty();
+        contextApiUrl.setType(ProviderConfigProperty.STRING_TYPE);
+        contextApiUrl.setName(CONF_CONTEXT_API_URL);
+        contextApiUrl.setLabel(CONF_CONTEXT_API_URL_LABEL);
+        contextApiUrl.setHelpText(CONF_CONTEXT_API_URL_HELPTEXT);
+
+        ProviderConfigProperty contextIssUrl = new ProviderConfigProperty();
+        contextIssUrl.setType(ProviderConfigProperty.STRING_TYPE);
+        contextIssUrl.setName(CONF_CONTEXT_ISS_URL);
+        contextIssUrl.setLabel(CONF_CONTEXT_ISS_URL_LABEL);
+        contextApiUrl.setHelpText(CONF_CONTEXT_ISS_URL_HELPTEXT);
+
+        List<ProviderConfigProperty> props = Arrays.asList(contextApiUrl, contextIssUrl);
+        return props;
     }
 
     @Override
