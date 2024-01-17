a1a199c7897a7dc9b9060ef6988c881f622802dd
bradhead <brad.head@gmail.com>
2024-01-17 13:33:51 -0800

    added handy shell script

---
+++ example-usage/restart.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Script to run the docker container including re-building the image, if necessary.
+
+@echo off
+echo "Stopping and removing keycloak container..."
+docker-compose down
+echo "Building and starting keycloak..."
+docker-compose build && docker-compose up -d
