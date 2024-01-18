6a64c44a7cd4289d89fd3484d5b75e680c4003a3
bradhead <brad.head@gmail.com>
2024-01-17 19:38:47 -0800

    added rebuild script

--- example-usage/reset.sh
+++ example-usage/reset.sh
@@ -7,4 +7,6 @@ docker-compose down
 echo "Building and starting keycloak..."
 docker-compose build && docker-compose up -d
 
+# okay, so now let's get terraform with realm-managmeent roles to 
+# be able to administer the realm.
 sh ./realm-admin.sh
