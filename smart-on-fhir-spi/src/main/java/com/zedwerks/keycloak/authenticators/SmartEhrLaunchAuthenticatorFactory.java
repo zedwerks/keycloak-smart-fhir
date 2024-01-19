16015be5848c25d12945d051f20c46ccabac2c66
bradhead <brad.head@gmail.com>
2024-01-19 10:42:28 -0800

    update

--- smart-on-fhir-spi/src/main/java/com/zedwerks/keycloak/authenticators/SmartEhrLaunchAuthenticatorFactory.java
+++ smart-on-fhir-spi/src/main/java/com/zedwerks/keycloak/authenticators/SmartEhrLaunchAuthenticatorFactory.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public class SmartEhrLaunchAuthenticatorFactory implements AuthenticatorFactory {
     private static final String PROVIDER_ID = "smart-ehr-launch";
-    private static final SmartEhrLaunchAuthenticator SINGLETON = new SmartEhrLaunchAuthenticator();
+    //private static final SmartEhrLaunchAuthenticator SINGLETON = new SmartEhrLaunchAuthenticator();
 
     // Configuration Settings to connect to the Context API server
     public static final String CONF_CONTEXT_API_URL = "context-api-url";
@@ -131,7 +131,7 @@ public class SmartEhrLaunchAuthenticatorFactory implements AuthenticatorFactory 
 
     @Override
     public Authenticator create(KeycloakSession session) {
-        return SINGLETON;
+        return new SmartEhrLaunchAuthenticator(session);
     }
 
     @Override
