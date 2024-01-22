1fd9ea362d8953b767a80149dcb9b0459de9f72b
bradhead <brad.head@gmail.com>
2024-01-22 10:13:41 -0800

    fixed resource name for fhircast stuff

--- example/terraform/scopes_smart_fhircast.tf
+++ example/terraform/scopes_smart_fhircast.tf
@@ -1,18 +1,18 @@
-resource "keycloak_openid_client_scope" "smart_context_read_scope" {
+resource "keycloak_openid_client_scope" "smart_fhircast_scope" {
   realm_id               = data.keycloak_realm.realm.id
   name                   = "fhircast"
   description            = "Permission to access fhirCast topics. This is an alternative to 'launch' scope in SMART on FHIR."
   include_in_token_scope = true
 }
 
-resource "keycloak_openid_client_scope" "smart_context_read_scope" {
+resource "keycloak_openid_client_scope" "smart_fhircast_patient_open_scope" {
   realm_id               = data.keycloak_realm.realm.id
   name                   = "Patient-open"
   description            = "Permission to open fhirCast topic against Patient."
   include_in_token_scope = true
 }
 
-resource "keycloak_openid_client_scope" "smart_context_read_scope" {
+resource "keycloak_openid_client_scope" "smart_fhircast_patient_close_scope" {
   realm_id               = data.keycloak_realm.realm.id
   name                   = "Patient-close"
   description            = "Permission to close fhirCast topic against Patient."
