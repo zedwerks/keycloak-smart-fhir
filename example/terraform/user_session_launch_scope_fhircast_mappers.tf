24ed969f41d5f94c20797cd4dd9806598c875724
bradhead <brad.head@gmail.com>
2024-02-07 14:26:48 -0800

    updates

--- example/terraform/user_session_launch_scope_fhircast_mappers.tf
+++ example/terraform/user_session_launch_scope_fhircast_mappers.tf
@@ -2,7 +2,7 @@ resource "keycloak_openid_user_session_note_protocol_mapper" "fhircast_hub_topic
   realm_id = data.keycloak_realm.realm.id
   client_scope_id     = keycloak_openid_client_scope.launch_context_scope.id
   name                = "user-session-fhircast-hub-topic-mapper"
-  claim_name          = "hub.topic"
+  claim_name          = "hub_topic" // Keycloak can't produce a "hub.topic" claim as a single key/value, so we use "hub_topic" instead
   claim_value_type    = "String"
   session_note        = "hub.topic"
   add_to_access_token = false
@@ -15,7 +15,7 @@ resource "keycloak_openid_user_session_note_protocol_mapper" "fhircast_hub_url_m
   realm_id            = data.keycloak_realm.realm.id
   client_scope_id     = keycloak_openid_client_scope.launch_context_scope.id
   name                = "user-session-fhircast-hub-url-mapper"
-  claim_name          = "hub.url"
+  claim_name          = "hub_url"   // Keycloak can't produce a "hub.url" claim as a single key/value, so we use "hub_url" instead
   claim_value_type    = "String"
   session_note        = "hub.url"
   add_to_access_token = false
