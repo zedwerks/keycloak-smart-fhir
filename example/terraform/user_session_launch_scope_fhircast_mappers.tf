24e23175ba0c9acf21d8f883ae2287e8b558a892
bradhead <brad.head@gmail.com>
2024-02-12 12:13:10 -0800

    updated session notes to use generic protocol mapper

--- example/terraform/user_session_launch_scope_fhircast_mappers.tf
+++ example/terraform/user_session_launch_scope_fhircast_mappers.tf
@@ -1,26 +1,41 @@
-resource "keycloak_openid_user_session_note_protocol_mapper" "fhircast_hub_topic_mapper" {
-  realm_id = data.keycloak_realm.realm.id
-  client_scope_id     = keycloak_openid_client_scope.launch_context_scope.id
-  name                = "user-session-fhircast-hub-topic-mapper"
-  claim_name          = "hub_topic" // Keycloak can't produce a "hub.topic" claim as a single key/value, so we use "hub_topic" instead
-  claim_value_type    = "String"
-  session_note        = "hub.topic"
-  add_to_access_token = false
-  add_to_id_token     = false
-  //add_to_access_token_response = true
+
+// Here, we are using the generic protocol mapper to add the hub.topic and hub.url claims to the user session note.
+// This is because this Terraform Provider doe not support setting the access token response claim to true for the user session note protocol mapper.
+resource "keycloak_generic_protocol_mapper" "fhircast_hub_topic_mapper" {
+  realm_id        = data.keycloak_realm.realm.id
+  client_scope_id = keycloak_openid_client_scope.launch_context_scope.id
+  name            = "user-session-fhircast-hub-topic-mapper"
+  protocol        = "openid-connect"
+  protocol_mapper = "oidc-usersessionmodel-note-mapper"
+  config = {
+    "user.session.note" = "hub.topic"
+    "introspection.token.claim" : "false",
+    "userinfo.token.claim" : "false",
+    "id.token.claim" : "false",
+    "access.token.claim" : "false",
+    "claim.name" : "hub_topic",
+    "jsonType.label" : "String",
+    "access.tokenResponse.claim" : "true"
+  }
   depends_on = [keycloak_openid_client_scope.launch_context_scope]
 }
 
-resource "keycloak_openid_user_session_note_protocol_mapper" "fhircast_hub_url_mapper" {
-  realm_id            = data.keycloak_realm.realm.id
-  client_scope_id     = keycloak_openid_client_scope.launch_context_scope.id
-  name                = "user-session-fhircast-hub-url-mapper"
-  claim_name          = "hub_url"   // Keycloak can't produce a "hub.url" claim as a single key/value, so we use "hub_url" instead
-  claim_value_type    = "String"
-  session_note        = "hub.url"
-  add_to_access_token = false
-  add_to_id_token     = false
-  //add_to_access_token_response = true
+resource "keycloak_generic_protocol_mapper" "fhircast_hub_url_mapper" {
+  realm_id        = data.keycloak_realm.realm.id
+  client_scope_id = keycloak_openid_client_scope.launch_context_scope.id
+  name            = "user-session-fhircast-hub-url-mapper"
+  protocol        = "openid-connect"
+  protocol_mapper = "oidc-usersessionmodel-note-mapper"
+  config = {
+   "user.session.note" = "hub.url"
+    "introspection.token.claim" : "false",
+    "userinfo.token.claim" : "false",
+    "id.token.claim" : "false",
+    "access.token.claim" : "false",
+    "claim.name" : "hub_url",
+    "jsonType.label" : "String",
+    "access.tokenResponse.claim" : "true"
+  }
   depends_on = [keycloak_openid_client_scope.launch_context_scope]
 }
 
--- example/terraform/user_session_launch_scope_mappers.tf
+++ example/terraform/user_session_launch_scope_mappers.tf
@@ -1,3 +1,8 @@
+// We need to use generic protocol mappers to add the user session note claims to the access token response.
+// The current edition of the Keycloak Terraform Provider does not support setting the access token response claim to true for the user session note protocol mapper.
+// The generic protocol mapper allows us to set the access token response claim to true.
+
+/*
 resource "keycloak_openid_user_session_note_protocol_mapper" "launch_smart_patient_mapper" {
   realm_id            = data.keycloak_realm.realm.id
   client_scope_id     = keycloak_openid_client_scope.launch_context_scope.id
@@ -9,44 +14,81 @@ resource "keycloak_openid_user_session_note_protocol_mapper" "launch_smart_patie
   add_to_id_token     = false
   //add_to_access_token_response = true
   depends_on = [keycloak_openid_client_scope.launch_context_scope]
-}
+} */
 
-resource "keycloak_openid_user_session_note_protocol_mapper" "smart_launch_patient_mapper" {
-  realm_id            = data.keycloak_realm.realm.id
-  client_scope_id     = keycloak_openid_client_scope.launch_patient_context_scope.id
-  name                = "user-session-patient-launch-mapper"
-  claim_name          = "patient"
-  claim_value_type    = "String"
-  session_note        = "patient"
-  add_to_access_token = false
-  add_to_id_token     = false
-  //add_to_access_token_response = true
+resource "keycloak_generic_protocol_mapper" "launch_smart_patient_mapper" {
+  realm_id        = data.keycloak_realm.realm.id
+  client_scope_id = keycloak_openid_client_scope.launch_context_scope.id
+  name            = "user-session-patient-mapper"
+  protocol        = "openid-connect"
+  protocol_mapper = "oidc-usersessionmodel-note-mapper"
+  config = {
+    "user.session.note" = "patient"
+    "introspection.token.claim" : "true",
+    "userinfo.token.claim" : "false",
+    "id.token.claim" : "false",
+    "access.token.claim" : "false",
+    "claim.name" : "patient",
+    "jsonType.label" : "String",
+    "access.tokenResponse.claim" : "true"
+  }
   depends_on = [keycloak_openid_client_scope.launch_context_scope]
 }
 
-resource "keycloak_openid_user_session_note_protocol_mapper" "launch_smart_encounter_mapper" {
-  realm_id            = data.keycloak_realm.realm.id
-  client_scope_id     = keycloak_openid_client_scope.launch_context_scope.id
-  name                = "user-session-encounter-mapper"
-  claim_name          = "encounter"
-  claim_value_type    = "String"
-  session_note        = "encounter"
-  add_to_access_token = false
-  add_to_id_token     = false
-  //add_to_access_token_response = true
+resource "keycloak_generic_protocol_mapper" "smart_launch_patient_scope_mapper" {
+  realm_id        = data.keycloak_realm.realm.id
+  client_scope_id = keycloak_openid_client_scope.launch_patient_context_scope.id
+  name            = "user-session-launch-patient-mapper"
+  protocol        = "openid-connect"
+  protocol_mapper = "oidc-usersessionmodel-note-mapper"
+  config = {
+    "user.session.note" = "patient"
+    "introspection.token.claim" : "true",
+    "userinfo.token.claim" : "false",
+    "id.token.claim" : "false",
+    "access.token.claim" : "true",
+    "claim.name" : "patient",
+    "jsonType.label" : "String",
+    "access.tokenResponse.claim" : "true"
+  }
   depends_on = [keycloak_openid_client_scope.launch_context_scope]
 }
 
-resource "keycloak_openid_user_session_note_protocol_mapper" "smart_launch_encounter_mapper" {
-  realm_id            = data.keycloak_realm.realm.id
-  client_scope_id     = keycloak_openid_client_scope.launch_encounter_context_scope.id
-  name                = "user-session-launch-encounter-mapper"
-  claim_name          = "encounter"
-  claim_value_type    = "String"
-  session_note        = "encounter"
-  add_to_access_token = false
-  add_to_id_token     = false
-  //add_to_access_token_response = true
+resource "keycloak_generic_protocol_mapper" "launch_smart_launch_encounter_scope_mapper" {
+  realm_id        = data.keycloak_realm.realm.id
+  client_scope_id = keycloak_openid_client_scope.launch_context_scope.id
+  name            = "user-session-encounter-mapper"
+  protocol        = "openid-connect"
+  protocol_mapper = "oidc-usersessionmodel-note-mapper"
+  config = {
+    "user.session.note" = "encounter"
+    "introspection.token.claim" : "true",
+    "userinfo.token.claim" : "false",
+    "id.token.claim" : "false",
+    "access.token.claim" : "false",
+    "claim.name" : "encounter",
+    "jsonType.label" : "String",
+    "access.tokenResponse.claim" : "true"
+  }
+  depends_on = [keycloak_openid_client_scope.launch_context_scope]
+}
+
+resource "keycloak_generic_protocol_mapper" "smart_launch_encounter_scope_mapper" {
+  realm_id        = data.keycloak_realm.realm.id
+  client_scope_id = keycloak_openid_client_scope.launch_encounter_context_scope.id
+  name            = "user-session-launch-encounter-mapper"
+  protocol        = "openid-connect"
+  protocol_mapper = "oidc-usersessionmodel-note-mapper"
+  config = {
+    "user.session.note" = "encounter"
+    "introspection.token.claim" : "true",
+    "userinfo.token.claim" : "false",
+    "id.token.claim" : "false",
+    "access.token.claim" : "true",
+    "claim.name" : "encounter",
+    "jsonType.label" : "String",
+    "access.tokenResponse.claim" : "true"
+  }
   depends_on = [keycloak_openid_client_scope.launch_context_scope]
 }
 
@@ -63,74 +105,99 @@ resource "keycloak_openid_user_session_note_protocol_mapper" "launch_smart_aud_m
 
 }
 
-resource "keycloak_openid_user_session_note_protocol_mapper" "launch_smart_fhir_context_mapper" {
-  realm_id            = data.keycloak_realm.realm.id
-  client_scope_id     = keycloak_openid_client_scope.launch_context_scope.id
-  name                = "user-session-fhir-context-mapper"
-  claim_name          = "fhirContext"
-  claim_value_type    = "String"
-  session_note        = "smart_fhir_context"
-  add_to_access_token = false
-  add_to_id_token     = false
-  //add_to_access_token_response = true
+resource "keycloak_generic_protocol_mapper" "launch_smart_fhir_context_mapper" {
+  realm_id        = data.keycloak_realm.realm.id
+  client_scope_id = keycloak_openid_client_scope.launch_context_scope.id
+  name            = "user-session-fhir-context-mapper"
+  protocol        = "openid-connect"
+  protocol_mapper = "oidc-usersessionmodel-note-mapper"
+  config = {
+    "user.session.note" = "smart_fhir_context"
+    "introspection.token.claim" : "true",
+    "userinfo.token.claim" : "false",
+    "id.token.claim" : "false",
+    "access.token.claim" : "false",
+    "claim.name" : "fhirContext",
+    "jsonType.label" : "String",
+    "access.tokenResponse.claim" : "true"
+  }
   depends_on = [keycloak_openid_client_scope.launch_context_scope]
-
 }
 
-resource "keycloak_openid_user_session_note_protocol_mapper" "launch_smart_patient_banner_mapper" {
-  realm_id            = data.keycloak_realm.realm.id
-  client_scope_id     = keycloak_openid_client_scope.launch_context_scope.id
-  name                = "user-session-patient-banner-mapper"
-  claim_name          = "need_patient_banner"
-  claim_value_type    = "String"
-  session_note        = "smart_need_patient_banner"
-  add_to_access_token = false
-  add_to_id_token     = false
-  //add_to_access_token_response = true
+resource "keycloak_generic_protocol_mapper" "launch_smart_patient_banner_mapper" {
+  realm_id        = data.keycloak_realm.realm.id
+  client_scope_id = keycloak_openid_client_scope.launch_context_scope.id
+  name            = "user-session-patient-banner-mapper"
+  protocol        = "openid-connect"
+  protocol_mapper = "oidc-usersessionmodel-note-mapper"
+  config = {
+    "user.session.note" = "smart_need_patient_banner"
+    "introspection.token.claim" : "false",
+    "userinfo.token.claim" : "false",
+    "id.token.claim" : "false",
+    "access.token.claim" : "false",
+    "claim.name" : "need_patient_banner",
+    "jsonType.label" : "String",
+    "access.tokenResponse.claim" : "true"
+  }
   depends_on = [keycloak_openid_client_scope.launch_context_scope]
-
 }
 
-resource "keycloak_openid_user_session_note_protocol_mapper" "launch_smart_intent_mapper" {
-  realm_id            = data.keycloak_realm.realm.id
-  client_scope_id     = keycloak_openid_client_scope.launch_context_scope.id
-  name                = "user-session-intent-mapper"
-  claim_name          = "intent"
-  claim_value_type    = "String"
-  session_note        = "smart_intent"
-  add_to_access_token = false
-  add_to_id_token     = false
-  //add_to_access_token_response = true
+resource "keycloak_generic_protocol_mapper" "launch_smart_intent_mapper" {
+  realm_id        = data.keycloak_realm.realm.id
+  client_scope_id = keycloak_openid_client_scope.launch_context_scope.id
+  name            = "user-session-intent-mapper"
+  protocol        = "openid-connect"
+  protocol_mapper = "oidc-usersessionmodel-note-mapper"
+  config = {
+    "user.session.note" = "smart_intent"
+    "introspection.token.claim" : "true",
+    "userinfo.token.claim" : "false",
+    "id.token.claim" : "false",
+    "access.token.claim" : "false",
+    "claim.name" : "intent",
+    "jsonType.label" : "String",
+    "access.tokenResponse.claim" : "true"
+  }
   depends_on = [keycloak_openid_client_scope.launch_context_scope]
-
 }
 
-resource "keycloak_openid_user_session_note_protocol_mapper" "launch_smart_style_mapper" {
-  realm_id            = data.keycloak_realm.realm.id
-  client_scope_id     = keycloak_openid_client_scope.launch_context_scope.id
-  name                = "user-session-smart-style-url-mapper"
-  claim_name          = "smart_style_url"
-  claim_value_type    = "String"
-  session_note        = "smart_style_url"
-  add_to_access_token = false
-  add_to_id_token     = false
-  //add_to_access_token_response = true
+resource "keycloak_generic_protocol_mapper" "launch_smart_style_mapper" {
+  realm_id        = data.keycloak_realm.realm.id
+  client_scope_id = keycloak_openid_client_scope.launch_context_scope.id
+  name            = "user-session-smart-style-url-mapper"
+  protocol        = "openid-connect"
+  protocol_mapper = "oidc-usersessionmodel-note-mapper"
+  config = {
+    "user.session.note" = "smart_style_url"
+    "introspection.token.claim" : "false",
+    "userinfo.token.claim" : "false",
+    "id.token.claim" : "false",
+    "access.token.claim" : "false",
+    "claim.name" : "smart_style_url",
+    "jsonType.label" : "String",
+    "access.tokenResponse.claim" : "true"
+  }
   depends_on = [keycloak_openid_client_scope.launch_context_scope]
-
 }
 
-resource "keycloak_openid_user_session_note_protocol_mapper" "launch_smart_tenant_mapper" {
-  realm_id            = data.keycloak_realm.realm.id
-  client_scope_id     = keycloak_openid_client_scope.launch_context_scope.id
-  name                = "user-session-tenant-mapper"
-  claim_name          = "tenant"
-  claim_value_type    = "String"
-  session_note        = "smart_tenant"
-  add_to_access_token = false
-  add_to_id_token     = false
-  //add_to_access_token_response = true
+resource "keycloak_generic_protocol_mapper" "launch_smart_tenant_mapper" {
+  realm_id        = data.keycloak_realm.realm.id
+  client_scope_id = keycloak_openid_client_scope.launch_context_scope.id
+  name            = "user-session-tenant-mapper"
+  protocol        = "openid-connect"
+  protocol_mapper = "oidc-usersessionmodel-note-mapper"
+  config = {
+    "user.session.note" = "smart_tenant"
+    "introspection.token.claim" : "true",
+    "userinfo.token.claim" : "false",
+    "id.token.claim" : "false",
+    "access.token.claim" : "false",
+    "claim.name" : "tenant",
+    "jsonType.label" : "String",
+    "access.tokenResponse.claim" : "true"
+  }
   depends_on = [keycloak_openid_client_scope.launch_context_scope]
-
 }
 
 
