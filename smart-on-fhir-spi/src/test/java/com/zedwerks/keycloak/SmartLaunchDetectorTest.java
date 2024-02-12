dd9737b57e3088efc4346a5bd8a7689f1509ff1a
bradhead <brad.head@gmail.com>
2024-02-12 09:59:29 -0800

    added invalid launch mock test.

--- smart-on-fhir-spi/src/test/java/com/zedwerks/keycloak/SmartLaunchDetectorTest.java
+++ smart-on-fhir-spi/src/test/java/com/zedwerks/keycloak/SmartLaunchDetectorTest.java
@@ -3,10 +3,12 @@ package com.zedwerks.keycloak;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
 
 import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.authentication.AuthenticationFlowError;
 import org.keycloak.authentication.Authenticator;
 import org.keycloak.models.AuthenticatorConfigModel;
 import org.keycloak.models.ClientModel;
@@ -14,7 +16,6 @@ import org.keycloak.models.ClientScopeModel;
 import org.keycloak.models.KeycloakContext;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.UserModel;
-import org.keycloak.protocol.oidc.TokenManager;
 import org.keycloak.sessions.AuthenticationSessionModel;
 import org.mockito.MockitoAnnotations;
 
@@ -24,6 +25,7 @@ import com.zedwerks.keycloak.authenticators.smart.SmartLaunchDetectorFactory;
 import jakarta.ws.rs.core.MultivaluedHashMap;
 import jakarta.ws.rs.core.MultivaluedMap;
 import jakarta.ws.rs.core.UriInfo;
+import jakarta.ws.rs.core.Response;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -153,5 +155,66 @@ public class SmartLaunchDetectorTest {
         // Test the authenticate method
         authenticator.authenticate(context);
         verify(context).attempted();
+    }
+
+    @Test
+    public void testInvalidLaunch() {
+        // Mock the necessary dependencies
+
+        KeycloakSession session = mock(KeycloakSession.class);
+        KeycloakContext keycloakContext = mock(KeycloakContext.class);
+        AuthenticationSessionModel authSession = mock(AuthenticationSessionModel.class);
+        AuthenticationFlowContext context = mock(AuthenticationFlowContext.class);
+        AuthenticatorConfigModel config = mock(AuthenticatorConfigModel.class);
+        UserModel user = mock(UserModel.class);
+        UriInfo uriInfo = mock(UriInfo.class);
+        ClientModel client = mock(ClientModel.class);
+        Authenticator authenticator = new SmartLaunchDetector(session);
+
+        SmartLaunchDetectorFactory factory = mock(SmartLaunchDetectorFactory.class);
+        when(factory.create(session)).thenReturn(authenticator);
+
+        MultivaluedMap<String, String> queryParameters = new MultivaluedHashMap<String, String>();
+        queryParameters.putSingle("aud", "https://fhir.example.com");
+        when(uriInfo.getQueryParameters()).thenReturn(queryParameters);
+        when(context.getUriInfo()).thenReturn(uriInfo);
+        when(context.getUriInfo().getQueryParameters()).thenReturn(queryParameters);
+
+
+        ClientScopeModel defaultScope = mock(ClientScopeModel.class);
+        ClientScopeModel profileScope = mock(ClientScopeModel.class);
+        ClientScopeModel launchScope = mock(ClientScopeModel.class);
+
+        when(profileScope.getName()).thenReturn("profile");
+        when(defaultScope.getName()).thenReturn("openid");
+        when(launchScope.getName()).thenReturn("launch");
+
+        Map<String, ClientScopeModel> allOptionalScopes = new HashMap<String, ClientScopeModel>();
+        allOptionalScopes.put("launch", launchScope);
+
+        when(client.getClientScopes(false)).thenReturn(allOptionalScopes);
+
+        Map<String, ClientScopeModel> defaultScopes = new HashMap<String, ClientScopeModel>();
+        defaultScopes.put("openid", defaultScope);
+        defaultScopes.put("profile", profileScope);
+
+        when(client.getClientScopes(true)).thenReturn(defaultScopes);
+
+        when(context.getAuthenticationSession()).thenReturn(authSession);
+        when(context.getAuthenticatorConfig()).thenReturn(config);
+        when(context.getUser()).thenReturn(user);
+        when(context.getSession()).thenReturn(session);
+        when(context.getUriInfo()).thenReturn(uriInfo);
+
+        when(session.getContext()).thenReturn(keycloakContext);
+        when(session.getContext().getAuthenticationSession()).thenReturn(authSession);
+        when(session.getContext().getClient()).thenReturn(client);
+
+        when(authSession.getClient()).thenReturn(client);
+        when(authSession.getClientNote("scope")).thenReturn("openid launch");
+
+        // Test the authenticate method.. this should fail.
+        authenticator.authenticate(context);
+        verify(context).failure(any(AuthenticationFlowError.class), any(Response.class));
     }
 }
