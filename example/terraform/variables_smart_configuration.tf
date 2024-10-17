2521bd160ae101eeee35497ee6542fe474719005
bradhead <brad.head@gmail.com>
2024-10-17 07:41:44 -0700

    fixed typo

--- example/terraform/variables_smart_configuration.tf
+++ example/terraform/variables_smart_configuration.tf
@@ -2,7 +2,7 @@ variable "keycloak_smart_configuration" {
   type = object({
     fhir_audiences = string
     fhircast_context_server_url = string
-    fhircat_context_server_scope = optional(string, "launch")
+    fhircast_context_server_scope = optional(string, "launch")
     fhircast_context_server_audience = optional(string, "context-service")
     fhircast_context_server_class_factory = optional(string)
   })
