51bea156b302efd7a6063190b8c982f40bf12d01
bradhead <brad.head@gmail.com>
2024-10-21 17:03:46 -0700

    updates

--- example/terraform/environments/demo/backend.tf
+++ example/terraform/environments/demo/backend.tf
@@ -1,29 +1,29 @@
 terraform {
   backend "s3" {
-    bucket         = "demo-terraform-state"  # Your bucket name
-    key            = "demo/terraform.tfstate"
-    region         = var.do_spaces_region  # Set your region (nyc3, sfo3, tor1, etc.)
-    endpoint       = var.do_spaces_endpoint  # DO endpoint for your region, provided in env variables
-    access_key     = var.do_spaces_access_key  # Provided through secrets variables
-    secret_key     = var.do_spaces_secret_key  # Provided through secrets variables
-    skip_credentials_validation = true  # Skips AWS-specific validation
-    skip_metadata_api_check     = true  # Skips AWS metadata API check
+    bucket                      = "demo-terraform-state" # Your bucket name
+    key                         = "demo/terraform.tfstate"
+    region                      = "tor1" # Set your region (nyc3, sfo3, tor1, etc.)
+    endpoint                    = ""     # DO endpoint for your region, provided in env variables
+    access_key                  = ""     # passed in from  secrets variables
+    secret_key                  = ""     # Provided through secrets variables
+    skip_credentials_validation = true   # Skips AWS-specific validation
+    skip_metadata_api_check     = true   # Skips AWS metadata API check
   }
 }
 
 variable "do_spaces_access_key" {
-  type = string
+  type    = string
   default = ""
 }
 variable "do_spaces_secret_key" {
-  type = string
+  type    = string
   default = ""
 }
 variable "do_spaces_endpoint" {
-  type = string
+  type    = string
   default = ""
 }
 variable "do_spaces_region" {
-  type = string
+  type    = string
   default = ""
 }
