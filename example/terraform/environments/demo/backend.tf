aa377b195ba5acfd288ac3016e93c0cfabf87e8d
bradhead <brad.head@gmail.com>
2024-10-21 23:37:35 -0700

    adds skips

--- example/terraform/environments/demo/backend.tf
+++ example/terraform/environments/demo/backend.tf
@@ -11,6 +11,8 @@ terraform {
     # Deactivate a few AWS-specific checks
     skip_credentials_validation = true
     skip_metadata_api_check     = true
+    skip_requesting_account_id = true
+    skip_region_validation = true
   }
 }
 
