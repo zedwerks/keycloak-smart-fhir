fd20785fc01c3a5920e509bc1e70ee2ecb31d6c0
bradhead <brad.head@gmail.com>
2024-10-21 23:41:31 -0700

    again

--- example/terraform/environments/demo/backend.tf
+++ example/terraform/environments/demo/backend.tf
@@ -11,8 +11,9 @@ terraform {
     # Deactivate a few AWS-specific checks
     skip_credentials_validation = true
     skip_metadata_api_check     = true
-    skip_requesting_account_id = true
-    skip_region_validation = true
+    skip_requesting_account_id  = true
+    skip_region_validation      = true
+    skip_s3_checksum            = true
   }
 }
 
