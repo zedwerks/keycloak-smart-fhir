6a03535dd9e3a8c21fb524e4f3d3569535c0dae9
bradhead <Brad.Head@gov.ab.ca>
2025-02-27 09:04:21 -0800

    update terraform

--- terraform/modules/smart_on_fhir/main.tf
+++ terraform/modules/smart_on_fhir/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     keycloak = {
-      source  = "mrparkers/keycloak"
-      version = ">= 4.4.0"
+      source  = "keycloak/keycloak"
+      version = ">= 5.0.0"
     }
   }
 }
