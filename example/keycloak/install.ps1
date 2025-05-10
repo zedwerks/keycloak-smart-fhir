b6a6cd2d42dd1aee245f39075c509329f833a854
Brad Head <38706615+bradhead@users.noreply.github.com>
2025-05-10 15:14:55 -0700

    update check for running

--- example/keycloak/install.ps1
+++ example/keycloak/install.ps1
@@ -5,23 +5,35 @@ $envFile = ".\config-env.sh"
 . "$PSScriptRoot\..\scripts\load-env.ps1" -envFile ${envFile}
 
 # Build Docker Compose services
-docker compose down -v --file .\docker-compose.yml
-if (docker compose --file .\docker-compose.yml build) {
+docker compose down -v 
+if (docker compose build) {
     Write-Host "Services built successfully. Now starting..."
-    docker compose --file .\docker-compose.yml up -d
+    docker compose up --detach
 } else {
-    Write-Host "Services failed to start."
+    Write-Host "Services failed to start." 
     exit 1
 }
 
 # Wait for Keycloak to be available
-Write-Host "Waiting for Keycloak to start..."
-while (-not (Invoke-WebRequest -Uri "http://localhost:8080")) {
-    Write-Host -NoNewline "."
-    Start-Sleep -Seconds 1
-}
-Write-Host "Keycloak is up and running."
 
+Write-Host "Keycloak is starting up."
+$GiveupSeconds = 120
+$EndTime = (Get-Date).AddSeconds($GiveupSeconds)
+$KeycloakUrl = "http://localhost:8080/"
+Write-Host "Waiting for Keycloak to be available at $KeycloakUrl."
+while ((Get-Date) -lt $EndTime) {
+    try {
+        $response = Invoke-WebRequest -Uri $KeycloakUrl -UseBasicParsing -TimeoutSec 5
+        if ($response.StatusCode -eq 200) {
+            Write-Host "."
+            Write-Host "Keycloak is available at $KeycloakUrl."
+            break
+        }
+    } catch {
+        Write-Host -NoNewLine "."
+        Start-Sleep -Seconds 2
+    }
+}
 # Execute realm permission setup inside the container
 Write-Host "Setting up permissions for the Terraform client..."
 docker exec -it keycloak-smart-fhir bash -c "/opt/keycloak/bin/terraform-realm-admin.sh"
